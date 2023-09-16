750e15420e5653c1411b5ad7b0334f3a952a5279
sergio15dev <sergio15.dev@gmail.com>
2023-09-15 22:09:51 -0500

    first commit

---
+++ Laboratorio_1/README.md
@@ -0,0 +1,8 @@
+# Laboratorio 1: La maldición de la dimensionalidad
+
+El presente informe tiene como objetivo analizar las distancias entre puntos en diferentes
+dimensiones, específicamente en dimensiones 10, 50, 100, 500, 1000, 2000, y 5000. Para llevar a cabo
+este análisis, se generaron histogramas que representan la distribución de estas distancias y se
+observaron las tendencias y cambios a medida que se aumenta la dimensión.
+
+## Informe: [Informe en PDF](informe.pdf)
---
+++ Laboratorio_1/histograma.py
@@ -0,0 +1,41 @@
+import matplotlib.pyplot as plt
+
+
+def graficarhistograma(dimension):
+    datos = []
+    dimensiones = {
+        2: [0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4],
+        10: [0, 0.25, 0.50, 0.75, 1.00, 1.25, 1.50, 1.75, 2.0],
+        50: [0, 1, 2, 3, 4, 5],
+        100: [0, 1, 2, 3, 4, 5],
+        500: [0, 2, 4, 6, 8, 10, 12],
+        1000: [0, 2, 4, 6, 8, 10, 12, 14],
+        2000: [0, 3, 6, 9, 12, 15, 18],
+        5000: [4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30]
+    }
+
+    valores_x = dimensiones.get(dimension, [])
+
+    # Abrir el archivo y leer los datos línea por línea
+    with open(f"distancias_dim{dimension}", 'r') as file:
+        for line in file:
+            dato = float(line.strip())
+            datos.append(dato)
+
+    # Crear el histograma
+    plt.hist(datos, bins=8, color='#69FF7B',
+             edgecolor='white', linewidth=3)
+    plt.grid(True)
+    plt.xlabel('Distancias')
+    plt.ylabel('Frecuencia')
+    plt.title(
+        f'Histograma de las distancias entre puntos con dimension {dimension}')
+    plt.xticks(valores_x)
+    plt.savefig(f"dimension{dimension}.png")
+
+
+with open('env', 'r') as archivo:
+    contenido = archivo.read()
+    dimension = int(contenido)
+
+graficarhistograma(dimension)
